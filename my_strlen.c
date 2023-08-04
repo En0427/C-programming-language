eac6adf175c60f16b98aa530bc9d20d40d6ea675
En0427 <125962979+En0427@users.noreply.github.com>
2023-08-04 18:33:58 +1000

    Add files via upload

---
+++ my_strlen.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <string.h>
+
+int my_strlen(char* str)
+{
+	int count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return count;
+}
+
+int main() 
+{
+	char arr[] = "bit";
+	
+	int len = my_strlen(arr);
+	printf("%d\n", len);
+
+	return 0;
+}
